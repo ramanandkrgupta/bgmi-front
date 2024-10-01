6bbf861ef3b106a367d8cd89a3fdcbd3f414c6b1
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-10-01 20:19:52 +0000

    FIX

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import axios from '../utils/axiosConfig'; // Assuming you have axios setup
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
