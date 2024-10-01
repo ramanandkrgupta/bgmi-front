b289659d6f05adf20d9f39f4960ac6afd91a945a
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-10-01 20:06:50 +0000

    backend api

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://bgmi-tournament-v1.onrender.com',
 });
 
 instance.interceptors.request.use((config) => {
