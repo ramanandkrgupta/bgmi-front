9a8c87bb60bbf6079de7d74edf7911d490332a22
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2024-10-01 19:44:07 +0000

    only front

--- models/Team.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TeamSchema = new mongoose.Schema({
-    name: { type: String, required: true, unique: true },
-    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
-});
-
-module.exports = mongoose.model('Team', TeamSchema);
